a8365a4c7f19bd708c7a3ebd6c87898cfe8f1b5a
Jon Bradley <weatchu@gmail.com>
2023-11-25 16:26:44 -0900

    wiki.vim use the projects .wiki directory

--- lua/user/wikivim.lua
+++ lua/user/wikivim.lua
@@ -5,9 +5,15 @@ local M = {
 }
 
 function M.config()
+  -- vim.g.wiki_root = '~/.wiki'
+  vim.g.wiki_root = WikiRoot()
+  vim.g.wiki_write_on_nav = 1
 end
 
-vim.g.wiki_root = '~/.wiki'
-vim.g.wiki_write_on_nav = 1
+-- Define a function to determine the wiki root dynmanically
+function WikiRoot()
+    local local_path = vim.fn.finddir('.wiki', ';./')
+    return not vim.fn.empty(local_path) and local_path or '~/.wiki'
+end
 
 return M
