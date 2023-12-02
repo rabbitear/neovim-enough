f5ce3eb1710adecade1d77caaa2ee99fd6381b9b
Jon Bradley <weatchu@gmail.com>
2023-12-02 00:58:31 -0900

    taskwiki is for vimwiki only :(

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,9 +1,5 @@
--- local M = {
---   "LunarVim/primer.nvim",
---   lazy = false, -- make sure we load this during startup if it is your main colorscheme
---   priority = 1000, -- make sure to load this before all the other start plugins
---   commit = "b8d7e08eed9a61eb2f49b9196b01f7f2932735ff",
--- }
+-- there is also kanagawa, woodwose suggests
+-- https://github.com/rebelot/kanagawa.nvim
 
 local M = {
   "morhetz/gruvbox",
---
+++ lua/user/taskwiki.lua
@@ -0,0 +1,16 @@
+-- this is only for Vimwiki 
+--    * should make this a branch, because doesn't work out-of-box
+--    * do we want to mod it?  To work with Wiki.vim ?
+
+local M = {
+  "tools-life/taskwiki",
+  dependencies = {
+    "powerman/vim-plugin-AnsiEsc",
+  },
+}
+
+function M.config()
+   -- do nothing
+end
+
+return M
